b4f8c949fa5f63b503f4d06036c8eb5f50a8455f
oldchevy <nick.olszowy@gmail.com>
2016-10-18 14:46:09 -0700

    (feat) Finish basic functionality

--- board.js
+++ board.js
@@ -1,8 +1,8 @@
 var colors = require('colors');
 
-var Board = function() {
+var Board = function(board) {
 
-  this._board = [
+  this._board = board || [
     [' ', ' ', ' '],
     [' ', ' ', ' '],
     [' ', ' ', ' '],
@@ -39,13 +39,147 @@ Board.prototype.insert = function(row, col, player) {
 };
 
 Board.prototype.checkPosition = function(row, col) {
-  return this._board[row][col] !== ' ';
+  if (row !== '' && col !== '') {
+    return this._board[row][col] === ' ';
+  } else {
+    return false;
+  }
 };
 
 Board.prototype.winChecker = function() {
 
+  var winner = this._rowChecker() ||
+               this._colChecker() ||
+               this._diagChecker() ||
+               this._fullChecker();
 
   return winner ? winner : undefined;
+};
+
+Board.prototype._rowChecker = function() {
+
+  var counters = {
+    X: 0,
+    O: 0
+  };
+  var result;
+
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      var position = this._board[row][col];
+      if (position !== ' ') {
+        counters[position]++;
+      }
+    }
+
+    //Check to see if a row was full of one thing
+    //and reset for the next row
+    for (var player in counters) {
+      if (counters[player] === 3) {
+        result = player;
+      }
+      counters[player] = 0;
+    }
+
+  }
+
+  // console.log('Inside row checker: ', counters);
+
+  return result;
+};
+
+Board.prototype._colChecker = function() {
+
+  var counters = {
+    X: 0,
+    O: 0
+  };
+  var result;
+
+  for (var col = 0; col < 3; col++) {
+    for (var row = 0; row < 3; row++) {
+      var position = this._board[row][col];
+      if (position !== ' ') {
+        counters[position]++;
+      }
+    }
+
+    //Check to see if a row was full of one thing
+    //and reset for the next row
+    for (var player in counters) {
+      if (counters[player] === 3) {
+        result = player;
+      }
+      counters[player] = 0;
+    }
+
+  }
+
+  // console.log('Inside col checker: ', counters);
+
+  return result;
+};
+
+Board.prototype._diagChecker = function() {
+
+  var rightcounters = {
+    X: 0,
+    O: 0
+  };
+  var leftcounters = {
+    X: 0,
+    O: 0
+  };
+
+  var result;
+
+  //Check right & left diagonal in the same loop
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      var position = this._board[row][col];
+      if (position !== ' ' && row === col) {
+        rightcounters[position]++;
+      }
+      if (position !== ' ' && row === 2 - col) {
+        leftcounters[position]++;
+      }
+    }
+  }
+
+  for (var player in rightcounters) {
+    if (rightcounters[player] === 3) {
+      result = player;
+    }
+  }
+
+  for (var player in leftcounters) {
+    if (leftcounters[player] === 3) {
+      result = player;
+    }
+  }
+
+  // console.log('Inside diag checker: ', leftcounters, rightcounters);
+
+  return result;
+};
+
+Board.prototype._fullChecker = function() {
+
+  var result = true;
+
+  for (var row = 0; row < 3; row++) {
+    for (var col = 0; col < 3; col++) {
+      var position = this._board[row][col];
+      if (position === ' ') {
+        result = false;
+        break;
+      }
+    }
+  }
+
+  // console.log('Inside full checker: ', '"' + result + '"');
+
+  return result;
 };
 
 module.exports = Board;
--- init.js
+++ init.js
@@ -2,9 +2,10 @@ var colors = require('colors');
 var Board = require('./board');
 var prompt = require('prompt');
 var game = new Board();
+var takenMessage = 'Enter a valid position that has not been taken plz';
+
 prompt.message = 'Enter a move Player 1!\n\n' + game.drawBoard();
-prompt.delimiter = ' => '.green;
-var player = false;
+prompt.delimiter = ' => '.blue;
 
 var properties = {
   properties: {
@@ -19,17 +20,59 @@ var properties = {
   }
 };
 
+var newGameProps = {
+  properties: {
+    decision: {
+      pattern: /^[y|n]$/,
+      message: '(Y/n)'
+    }
+  }
+};
 
-var getMove = function(player) {
+var newGame = function() {
+  prompt.message = 'Would you like to play another game?';
+  prompt.get(newGameProps, function(err, results) {
+    if (results.decision === 'y') {
+      game = new Board();
+      getMove(true);
+    } 
+  });
+
+};
+
+var getMove = function(player, badMove) {
 
   var num = player ? '1' : '2';
-  prompt.message = 'Enter a move Player ' + num + '!\n\n' + game.drawBoard();
+  console.log('\n\n' + game.drawBoard());
+  prompt.message = badMove 
+                    ? takenMessage 
+                    : 'Enter a move Player ' + num + '!';
+
   prompt.get(properties, function(err, results) {
-    game.insert(results.row, results.col, player);
-    player = !player;
-    getMove(player);
+
+    //check if the move was valid
+    if (game.checkPosition(results.row, results.col)) {
+      
+      game.insert(results.row, results.col, player);
+
+      //check the game conditions for a win or draw
+      var winner = game.winChecker();
+      if (winner) {
+        winner === true 
+          ? console.log('\n\nDraw -\n\n', game.drawBoard())
+          : console.log('\n\nWinner is ' + winner + '!\n\n', game.drawBoard());
+        newGame();
+      } else {      
+        player = !player;
+        getMove(player, false);
+      }
+
+    //If the move isn't valid, change the prompt and not the player
+    } else {
+      getMove(player, true);
+    }
   });
 };
 
 prompt.start();
-getMove();
+getMove(true);
